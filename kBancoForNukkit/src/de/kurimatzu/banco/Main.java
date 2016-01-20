3065cafc0170c6ef2129d93c557006c1744536f4
Kurimatzu <davirocha20@hotmail.com.br>
2016-01-20 16:46:01 -0200

    'Deixando' o codigo direito.

--- kBancoForNukkit/src/de/kurimatzu/banco/Main.java
+++ kBancoForNukkit/src/de/kurimatzu/banco/Main.java
@@ -11,9 +11,9 @@ import cn.nukkit.plugin.PluginBase;
 
 
 public class Main extends PluginBase implements Listener{
-	
+
 	public static Plugin instance;
-	
+
 	public void onEnable() {
 		this.getDataFolder().mkdirs();
 		instance = this;
@@ -27,7 +27,7 @@ public class Main extends PluginBase implements Listener{
 			Utils.criarConta(e.getPlayer());
 		}
 	}
-	
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if(!(sender instanceof Player)) {
 			sender.sendMessage("§cComando somente no servidor."); 
@@ -42,34 +42,34 @@ public class Main extends PluginBase implements Listener{
 				return true;
 			}
 			if(args.length == 2) {
-               if(args[0].contains("pay")) {
+				if(args[0].contains("pay")) {
 					player.sendMessage("§c[Money] §a/money pay <nick> <quantia>");
 					return true;
 				}
-               
+
 			}
 			if(args.length == 3) {
-		 if(args[0].contains("pay")) {
-				String jogador = args[1];
-				int quantia = Integer.parseInt(args[2]);
-				if(getServer().getPlayer(jogador) != null) {
-					getServer().getPlayer(jogador).sendMessage("§c[Money] §a" + player.getName() + " §cenviou a você §a" + quantia + " coins.");
-				   Utils.addMoney(getServer().getPlayer(jogador), quantia);
-					return true;
-				} else {
-					player.sendMessage("§cJogador Offline.");
-					return true;
+				if(args[0].contains("pay")) {
+					String jogador = args[1];
+					int quantia = Integer.parseInt(args[2]);
+					if(getServer().getPlayer(jogador) != null) {
+						getServer().getPlayer(jogador).sendMessage("§c[Money] §a" + player.getName() + " §cenviou a você §a" + quantia + " coins.");
+						Utils.addMoney(getServer().getPlayer(jogador), quantia);
+						return true;
+					} else {
+						player.sendMessage("§cJogador Offline.");
+						return true;
+					}
 				}
-				  }
 			}
-			
+
 			if(Utils.exists(player)) {
 				player.sendMessage("§c[Money] §aSeu dinheiro e de:" + " " + Utils.getMoney(player));				
-				
-	}
-	
+
+			}
+
 		}
-		
+
 		return false;
 	}
 }
