5a062517ebccded0cc242da3c11f2c1fabf77a3e
Kurimatzu <Davi@192.168.0.2>
2016-01-20 07:42:25 -0200

    Plugin Iniciado.

---
+++ kVautForNukkit/src/de/kurimatzu/banco/Main.java
@@ -0,0 +1,48 @@
+package de.kurimatzu.banco;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.Listener;
+import cn.nukkit.event.player.PlayerJoinEvent;
+import cn.nukkit.plugin.Plugin;
+import cn.nukkit.plugin.PluginBase;
+
+
+public class Main extends PluginBase implements Listener{
+	
+	public static Plugin instance;
+	
+	public void onEnable() {
+		this.getDataFolder().mkdirs();
+		instance = this;
+		getServer().getPluginManager().registerEvents(this, this);
+		Utils.c.save();
+		Utils.c.save(true);
+	}
+	@EventHandler
+	public void join(PlayerJoinEvent e) {
+		if(!Utils.exists(e.getPlayer())) {
+			Utils.criarConta(e.getPlayer());
+		}
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(!(sender instanceof Player)) {
+			sender.sendMessage("§cComando somente no servidor."); 
+			return true;
+		}
+		Player player = (Player)sender;
+		if(cmd.getName().toLowerCase().contains("money")) {
+			if(Utils.exists(player)) {
+				player.sendMessage("§c[Money] §aSeu dinheiro e de:" + " " + Utils.getMoney(player));				
+				
+	}
+		return false;
+		}
+		return false;
+	}
+}
+
+
---
+++ kVautForNukkit/src/de/kurimatzu/banco/Utils.java
@@ -0,0 +1,48 @@
+package de.kurimatzu.banco;
+
+import java.io.File;
+
+import cn.nukkit.Player;
+import cn.nukkit.utils.Config;
+
+public class Utils {
+
+	public static Config c = new Config(new File(Main.instance.getDataFolder(), "contas.yml"));
+	
+	
+	public static double getMoney(final Player p) {
+		if(c.exists(p.getUniqueId().toString())) {
+			return (double) c.get(p.getUniqueId().toString());
+		}
+		return 0;
+		
+	}
+	public static void deletarConta(final Player p) {
+		if(c.exists(p.getUniqueId().toString())) {
+			c.remove(p.getUniqueId().toString());
+			c.save();
+			c.save(true);
+		} else {
+			p.sendMessage("§cops, essa conta nao existe.");
+		}
+	}
+	public static void criarConta(final Player p) {
+		if(!c.exists(p.getUniqueId().toString())) {
+			c.set(p.getUniqueId().toString(), Double.valueOf(0));
+			c.save();
+			c.save(true);
+		} else {
+			p.sendMessage("§cops, essa conta já existe.");
+		}
+	}
+	public static boolean exists(final Player p) {
+		if(c.exists(p.getUniqueId().toString())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	
+	
+}
